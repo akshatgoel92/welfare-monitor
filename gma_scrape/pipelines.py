ad5392af1bd4f2d519cbe6fe96ad7c90b9cd1f1f
akshat92 <akshatgoel92@users.noreply.github.com>
2018-08-16 13:02:42 +0530

    Added processing pipe-line for FTO number item

--- gma_scrape/pipelines.py
+++ gma_scrape/pipelines.py
@@ -6,6 +6,61 @@
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
 
-class GmaScrapePipeline(object):
-    def process_item(self, item, spider):
-        return item
+# Import packages
+from scrapy.contrib.exporter import CsvItemExporter
+from gma_scrape.items import NREGAItem
+from gma_scrape.items import FTONo
+
+# FTO number pipe-line   	
+class FTOSummaryPipeline(object):
+	
+	def open_spider(self, spider):
+		
+		self.file = open('fto_summary.csv', 'w+b')
+		
+		self.exporter = CsvItemExporter(self.file)
+		
+		self.exporter.start_exporting()
+	
+	def process_item(self, item, spider):
+   		
+   		if isinstance(item, NREGAItem):
+   		
+   			self.exporter.export_item(item)
+   			
+   		return(item)
+   	
+	def close_spider(self, spider):
+   		
+   		self.exporter.finish_exporting()
+   		
+   		self.file.close()
+   		   		
+# FTO number pipe-line   	
+class FTONoPipeline(object):
+	
+	def open_spider(self, spider):
+		
+		self.file = open('fto_numbers.csv', 'w+b')
+		
+		self.exporter = CsvItemExporter(self.file)
+		
+		self.exporter.start_exporting()
+	
+	def process_item(self, item, spider):
+   		
+   		if isinstance(item, FTONo):
+   		
+   			self.exporter.export_item(item)
+   			
+   		return(item)
+   	
+	def close_spider(self, spider):
+   		
+   		self.exporter.finish_exporting()
+   		
+   		self.file.close()
+	
+	
+    	
+	
