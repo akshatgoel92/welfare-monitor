612d755125e8952df053a2edba25c6bd5d6e1809
akshat92 <akshatgoel92@users.noreply.github.com>
2018-09-03 01:58:49 +0530

    Added in AWS SQL pipeline for FTO numbers and FTO content spiders to pipelines file

--- nrega_scrape/pipelines.py
+++ nrega_scrape/pipelines.py
@@ -20,7 +20,7 @@ from common.helpers import sql_connect
 pymysql.install_as_MySQLdb()
 
 	
-# FTO number pipe-line   	
+# FTO number pipe-line		
 class FTOSummaryPipeline(object):
 	
 	def __init__(self):
@@ -36,50 +36,50 @@ class FTOSummaryPipeline(object):
 		args = (
 		
 					item['block_name'].encode('utf-8'),
-    
-    				item['total_fto'].encode('utf-8'),
-         
-    				item['first_sign'].encode('utf-8'),
-    
-    				item['first_sign_pending'].encode('utf-8'),
-    
-    				item['second_sign'].encode('utf-8'),
-    
-    				item['second_sign_pending'].encode('utf-8'),
-    
-    				item['fto_sent_bank'].encode('utf-8'),
-    
-    				item['transact_sent_bank'].encode('utf-8'),
-    
-    				item['fto_processed_bank'].encode('utf-8'),
-    
-    				item['transact_processed_bank'].encode('utf-8'),
-    
-    				item['fto_partial_bank'].encode('utf-8'),
-    
-    				item['transact_partial_bank'].encode('utf-8'),
-    
-    				item['fto_pending_bank'].encode('utf-8'),
-    
-    				item['transact_pending_bank'].encode('utf-8'),
-    
-    				item['transact_processed_bank_resp'].encode('utf-8'),
-    
-    				item['invalid_accounts_bank_resp'].encode('utf-8'),
-    
-    				item['transact_rejected_bank_resp'].encode('utf-8'),
-    
-    				item['transact_total_bank_resp'].encode('utf-8'),
-    	
-    				item['url'].encode('utf-8'),
-    		
-    				item['spider'].encode('utf-8'),
-    
-    				item['server'].encode('utf-8'),
-    
-    				item['date'].encode('utf-8')
-    				
-    				)
+	
+					item['total_fto'].encode('utf-8'),
+		 
+					item['first_sign'].encode('utf-8'),
+	
+					item['first_sign_pending'].encode('utf-8'),
+	
+					item['second_sign'].encode('utf-8'),
+	
+					item['second_sign_pending'].encode('utf-8'),
+	
+					item['fto_sent_bank'].encode('utf-8'),
+	
+					item['transact_sent_bank'].encode('utf-8'),
+	
+					item['fto_processed_bank'].encode('utf-8'),
+	
+					item['transact_processed_bank'].encode('utf-8'),
+	
+					item['fto_partial_bank'].encode('utf-8'),
+	
+					item['transact_partial_bank'].encode('utf-8'),
+	
+					item['fto_pending_bank'].encode('utf-8'),
+	
+					item['transact_pending_bank'].encode('utf-8'),
+	
+					item['transact_processed_bank_resp'].encode('utf-8'),
+	
+					item['invalid_accounts_bank_resp'].encode('utf-8'),
+	
+					item['transact_rejected_bank_resp'].encode('utf-8'),
+	
+					item['transact_total_bank_resp'].encode('utf-8'),
+		
+					item['url'].encode('utf-8'),
+			
+					item['spider'].encode('utf-8'),
+	
+					item['server'].encode('utf-8'),
+	
+					item['date'].encode('utf-8')
+					
+					)
 		
 		sql = """ INSERT INTO fto_summary (block_name, first_sign, first_sign_pending, fto_partial_bank, 
 					 fto_pending_bank, fto_processed_bank, fto_sent_bank, invalid_accounts_bank_resp, second_sign, 
@@ -98,65 +98,132 @@ class FTOSummaryPipeline(object):
 			self._insert_record(item)
 			
 		return(item)
-   		   		
-# FTO number pipe-line   	
+
+
+
 class FTONoPipeline(object):
 	
-	def open_spider(self, spider):
+	def __init__(self):
+		
+		user, password, host, db = sql_connect().values()
+		
+		self.conn = pymysql.connect(host, user, password, db, charset = "utf8", use_unicode = True)
+		
+		self.cursor = self.conn.cursor()
+		
 	
-		if spider.name == 'fto_stats':
+	def _insert_record(self, item):
 		
-			self.file = open('fto_numbers.csv', 'w+b')
+		args = (
+					
+					item['fto_no'].encode('utf-8'),
+					
+					item['state_code'].encode('utf-8'),
+					
+					item['district_code'].encode('utf-8'), 
+					
+					item['block_code'].encode('utf-8'), 
+					
+					item['process_date'],
+					
+					item['url'].encode('utf-8'),
+					
+					item['spider'].encode('utf-8'),
+					
+					item['server'].encode('utf-8'),
+					
+					item['date'].encode('utf-8'),
+					
+					item['fto_stage'].encode('utf-8')
+					
+					)
+					
+		sql = """INSERT INTO fto_numbers (fto_no, state_code, district_code, block_code, process_date,
+					 url, spider, server, date, fto_stage) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s) """
+					 
+		self.cursor.execute(sql, args)
 		
-			self.exporter = CsvItemExporter(self.file)
+		self.conn.commit()
 		
-			self.exporter.start_exporting()
-	
-	def close_spider(self, spider):
-   		
-   		if spider.name == 'fto_stats':
-   		
-   			self.exporter.finish_exporting()
-   		
-   			self.file.close()
-   		
 	def process_item(self, item, spider):
-   		
-   		if isinstance(item, FTONo):
-   			
-   			self.exporter.export_item(item)
-   		
-   		return(item)
-   		
-# FTO Content Processor		
-class FTOContentPipeline(object):
-	
-	def open_spider(self, spider):
-	
-		if spider.name == 'fto_content':
 		
-			self.file = open('fto_content.csv', 'w+b')
+		if isinstance(item, FTONo):
 			
-			self.exporter = CsvItemExporter(self.file)
-			
-			self.exporter.start_exporting()
-		
-	def close_spider(self, spider):
-	
-		if spider.name == 'fto_content':
-		
-			self.exporter.finish_exporting()
-			
-			self.file.close()
-
-	def process_item(self, item, spider):
-	
-		if isinstance(item, FTOItem):
-		
-			self.exporter.export_item(item)
+			self._insert_record(item)
 			
 		return(item)
 		
+		
+class FTOContentPipeline(object):
+	
+	def __init__(self):
+		
+		user, password, host, db = sql_connect().values()
+		
+		self.conn = pymysql.connect(host, user, password, db, charset="utf8", use_unicode=True)
+		
+		self.cursor = self.conn.cursor()
+		
+	def _insert_record(self, item):
+	
+		args = (
+					
+					item['block_name'].encode('utf-8'),
+					
+					item['jcn'].encode('utf-8'),
+					
+					item['transact_ref_no'].encode('utf-8'),
+					
+					item['transact_date'].encode('utf-8'),
+					
+					item['app_name'].encode('utf-8'),
+					
+					item['prmry_acc_holder_name'].encode('utf-8'),
+					
+					item['wage_list_no'].encode('utf-8'),
+					
+					item['acc_no'].encode('utf-8'),
+					
+					item['bank_code'].encode('utf-8'),
+					
+					item['ifsc_code'].encode('utf-8'),
+					
+					item['credit_amt_due'].encode('utf-8'),
+					
+					item['status'].encode('utf-8'),
+					
+					item['processed_date'].encode('utf-8'),
+					
+					item['utr_no'].encode('utf-8'),
+					
+					item['rejection_reason'].encode('utf-8'),
+					
+					item['server'].encode('utf-8'),
+					
+					item['scrape_date'].encode('utf-8'),
+					
+					item['time_taken'].encode('utf-8'),
+					
+					item['url'].encode('utf-8')
+				
+					)
+					
+		sql = """ INSERT INTO fto_content (block_name, jcn, transact_ref_no, transact_date, app_name,
+					  prmry_acc_holder_name, wage_list_no, acc_no, bank_code, ifsc_code, credit_amt_due, 
+					  status, processed_date, utr_no, rejection_reason, server, scrape_date, time_taken, url)
+					  VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s) """
+					  
+		
+	def process_item(self, item, spider):
+		
+		if isinstance(item, FTOItem):
+			
+			self._insert_record(self, item)
+			
+		return(item)
+		
+			
+	
 
 
 	
