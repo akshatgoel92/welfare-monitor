92eb6cbf0721458b8c9094e75e0c3eb3e9b1c7f4
akshatgoel92 <akshatgoel92@users.noreply.github.com>
2018-09-16 12:02:13 +0530

    Added list of error e-mail recipients to backend and added e-mail functionality from G-mail to common modules

--- common/helpers.py
+++ common/helpers.py
@@ -2,6 +2,13 @@
 import os
 import json 
 import pymysql
+
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
+from email import encoders
+
 from twisted.enterprise import adbapi
 from sqlalchemy import *
 
@@ -16,6 +23,41 @@ def sql_connect():
 		sql_access = json.load(secrets)['mysql']
 		
 	return(sql_access)
+	
+# Send an attachment via e-mail	
+def send_file(file, subject, recipients):
+	
+	with open('./gma_secrets.json') as secrets:
+		
+		mail_access = json.load(secrets)['e-mail']
+	
+	msg = MIMEMultipart()
+	
+	msg['Subject'] = subject
+	
+	msg['From'] = 'python@python.com'
+	
+	msg['To'] = ', '.join(recipients)
+	
+	part = MIMEBase('application', "octet-stream")
+	
+	part.set_payload(open(file, "rb").read())
+	
+	encoders.encode_base64(part)
+	
+	part.add_header('Content-Disposition', 'attachment; filename=' + file)
+	
+	msg.attach(part)
+	
+	s = smtplib.SMTP('smtp.gmail.com' , 587)
+	
+	s.starttls()
+	
+	s.login(mail_access['user_name'], mail_access['password'])
+
+	s.sendmail(mail_access['user_name'], recipients, msg.as_string())
+	
+	s.quit()
 	
    
    
