24fd70e4bde85bb48eb78520427e5d224d770361
akshatgoel92 <akshatgoel92@users.noreply.github.com>
2018-09-22 01:44:23 +0530

    Added an EC2 setup file which takes care of package installation too

--- backend/ec2/install_packages.sh
+++
@@ -1,7 +0,0 @@
-#
-#  Original solution via StackOverflow:
-#    https://stackoverflow.com/questions/7225900/how-to-install-packages-using-pip-according-to-the-requirements-txt-file-from-a
-#
-
-
-pip install -r /home/ec2-user/gma-scrape/backend/ec2/requirements.txt
---
+++ backend/ec2/setup.sh
@@ -0,0 +1,30 @@
+# Install conda and make it system Python 
+wget http://repo.continuum.io/archive/Anaconda3-5.2.0-Linux-x86_64.sh
+bash Anaconda3-5.2.0-Linux-x86_64.sh
+source .bashrc
+
+# Install git
+sudo yum install git
+
+# Clone project
+git clone https://github.com/hks-epod/gma-scrape.git
+
+# Install pip
+pip install --upgrade pip
+
+# Install packages listed in requirements.txt
+pip install -r /home/ec2-user/gma-scrape/backend/ec2/requirements.txt
+
+# Make directory for log files
+mkdir nrega_output
+
+# Get Chrome driver
+wget https://chromedriver.storage.googleapis.com/2.42/chromedriver_linux64.zip
+unzip chromedriver_linux64.zip
+sudo mv chromedriver /usr/bin/chromedriver
+
+# Get Google Chrome
+cd /usr/bin/
+curl https://intoli.com/install-google-chr
+sudo mv /usr/bin/google-chrome-stable /usr/bin/google-chrome
+google-chrome --version && which google-chrome
