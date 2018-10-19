b6d439da045454ee9d96c74d1b09ed85234e3887
akshatgoel92 <akshatgoel92@users.noreply.github.com>
2018-10-19 12:39:39 +0530

    Adding crontab schedule

--- backend/scheduler/cron_scheduler.py
+++ backend/scheduler/cron_scheduler.py
@@ -4,8 +4,8 @@ from crontab import CronTab
 # Schedule job to run 730 am every weekday
 my_cron = CronTab()
 # Create a cron job    
-job = my_cron.new(command='./nrega_etl.sh')
+job = my_cron.new(command='scrapy crawl fto_content')
 # Set the job schedule
-job.setall('30 7 * 1-5 *')
+job.setall('41 09-18 * * *')
 # Write the job to the crontab 
 my_cron.write()
