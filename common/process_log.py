01ba05d9bd4d2f811d56ef7ae5a90b86c7811a26
akshatgoel92 <akshatgoel92@users.noreply.github.com>
2018-10-20 14:17:06 +0530

    Moved os.link log removal command from main execution code block to inside function in log processing file

--- common/process_log.py
+++ common/process_log.py
@@ -20,9 +20,10 @@ def process_log():
 	# Upload log file and send notification
 	helpers.dropbox_upload('./nrega_output/log.csv', '/Logs/log_' + now + '.csv')
 	helpers.send_email('Done, check the log!', 'GMA NREGA Scrape: Error count', recipients)
+	os.unlink('./nrega_output/log.csv')
 
 if __name__ == '__main__':
 
 	# Execute the processing	
 	process_log()
-	os.unlink('./nrega_output/log.csv')
+	
