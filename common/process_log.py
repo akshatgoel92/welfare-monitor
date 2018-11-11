2717dc2745ac5f6685574a934220f2d4eaa42a90
akshatgoel92 <akshatgoel92@users.noreply.github.com>
2018-11-11 19:00:13 +0530

    Removed process function code from process_log.py and added it to helpers instead

--- common/process_log.py
+++ common/process_log.py
@@ -1,28 +1,8 @@
 # Import packages
-import os
-import datetime
-import pandas as pd
-import numpy as np
-import json
 import helpers
 
-
-# Process the log
-def process_log():
-	
-	# Store recipients
-	with open('./backend/mail/recipients.json') as r:
-		recipients = json.load(r)
-	
-	# Store date 
-	now = str(datetime.datetime.now())
-	
-	# Upload log file and send notification
-	helpers.dropbox_upload('./nrega_output/log.csv', '/Logs/log_' + now + '.csv')
-	os.unlink('./nrega_output/log.csv')
-
+# Execute the processing
 if __name__ == '__main__':
 
-	# Execute the processing	
-	process_log()
-	
+	helpers.process_log()
+	helpers.update_fto_nos()
