e3f02a4f78e7eea0c06724cf9d9b1fcc66288252
akshatgoel92 <akshatgoel92@users.noreply.github.com>
2018-11-11 18:42:31 +0530

    Made code style and formatting changes in helpers file

--- common/helpers.py
+++ common/helpers.py
@@ -30,63 +30,63 @@ def sql_connect():
 	return(sql_access)
 
 # Return connection and cursor
+# Store credentials
+# Create connection	
 def db_conn():
 	
 	with open('./gma_secrets.json') as secrets:
 		# This gets credentials
 		sql_access = json.load(secrets)['mysql']
 	
-	# Store credentials
 	user = sql_access['username']
 	password = sql_access['password']
 	host = sql_access['host']
 	db = sql_access['db']
 	
-	# Create connection	
 	conn = pymysql.connect(host, user, password, db, charset="utf8", use_unicode=True)
 	cursor = conn.cursor()
 	
-	# Return statement
 	return(conn, cursor)
 	
-	
+# Get rid of surrounding white-space for string variables
+# Convert whatever fields that we can into title-case
 def clean_item(item, title_fields):
 	
 	for field in item.keys():
-		# Get rid of surrounding white-space for string variables
 		item[field] = item[field].strip() if type(item[field]) == str else item[field]
 		
-		# Convert whatever fields that we can into title-case
 		if field in title_fields:
 			item[field] = item[field].title()
 	
-	# Return statement
 	return(item)
 
 # Get a table's keys
+# Construct SQL table and return
 def get_keys(table):
 	
-	# Construct SQL table and return
+	
 	with open('./backend/db/table_keys.json') as file:
 		tables = json.load(file)
 		keys = tables[table]
-	# Return statement	
+	
 	return(keys)
 	
 # Insert a record into the SQL data-base
+# Get the inputs to the SQL command
+# Construct the SQL command and return
 def insert_data(item, keys, table, unique = 0):
 		
-	# Get the inputs to the SQL command
+	
 	keys = get_keys(table)
 	fields = u','.join(keys)
 	qm = u','.join([u'%s'] * len(keys))
 	sql = "INSERT INTO " + table + " (%s) VALUES (%s)"
 	sql_unique = "INSERT IGNORE INTO " + table + " (%s) VALUES (%s)"
 	
-	# Construct the SQL command and return
 	insert = sql if unique == 0 else sql_unique
 	sql = insert % (fields, qm)
 	data = [item[k] for k in keys]
+
 	return(sql, data)
 
 # Send e-mail
@@ -123,17 +123,18 @@ def send_email(msg, subject, recipients):
 	conn.sendmail(sender, recipients, msg.as_string())
 	conn.close()
 
-# Upload file to Dropbox 	
+# Upload file to Dropbox
+# Open the credentials JSON file
+# Store credentials and create Dropbox object
+# Upload the file with option to over-write if it already exists	
 def dropbox_upload(file_from, file_to):
     
     with open('./gma_secrets.json') as data_file:
         credentials = json.load(data_file)
 	
-	# Store credentials and create Dropbox object
     access_token = credentials['dropbox']['access_token']
     dbx = dropbox.Dropbox(access_token)
     
-    # Upload the file with option to over-write if it already exists
     with open(file_from, 'rb') as f:
         dbx.files_upload(f.read(), 
                          file_to, 
