d21c0fe16a83e7d9983e429e71d404d76528d78c
akshatgoel92 <akshatgoel92@users.noreply.github.com>
2018-11-24 12:54:29 +0530

    Reformatted and added comments to nrega_etl shell script

--- shell/nrega_etl.sh
+++ shell/nrega_etl.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
 # Execute the ETL
+# Switch to the working directory
 cd /home/ec2-user/fba-bank-scrape/
 
+# Then execute the content spider
 scrapy crawl fto_content
 
+# Then update the queue in the SQL database 
+# Then update the log
 python ./common/update_ftos.py
-
 python ./common/process_log.py
