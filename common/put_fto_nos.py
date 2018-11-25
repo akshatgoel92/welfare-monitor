18d08b14edb7dfcb946552ff6424ee2897e89942
akshatgoel92 <akshatgoel92@users.noreply.github.com>
2018-11-25 14:41:42 +0530

    Added tested put_fto_nos module to common folder

--- common/put_fto_nos.py
+++ common/put_fto_nos.py
@@ -1,17 +1,18 @@
 # Imports
+import sys
 import os
 import pandas as pd
 import numpy as np
 import pymysql
+import argparse
 from sqlalchemy import *
 from sqlalchemy.engine import reflection
-pymysql.install_as_MySQLdb()
 
 # Import item files
-from nrega_scrape.items import FTOItem
-from nrega_scrape.items import NREGAItem
-from nrega_scrape.items import FTONo
-from common.helpers import sql_connect
+from helpers import sql_connect
+
+# Install this as MySQLdb
+pymysql.install_as_MySQLdb()
 
 # Put FTO queue in a table
 def put_fto_nos(table, engine, path):
@@ -19,13 +20,18 @@ def put_fto_nos(table, engine, path):
     fto_nos = pd.read_excel(path).drop_duplicates()
     fto_nos['done'] = 0
     fto_nos['fto_type'] = ''
-    fto_nos.to_sql(table, con = engine, index = False, if_exists = 'replace')
+    fto_nos.to_sql(table, 
+					con = engine, 
+					index = False, 
+					if_exists = 'replace')
 
 if __name__ == '__main__':
 
 	 # Create parser
 	parser = argparse.ArgumentParser(description='Parse the block')
-	parser.add_argument('block', type=str, help='Block name', required = True)
+	parser.add_argument('block', 
+						type=str,
+						help='Block name')
 
 	# Parse arguments
 	args = parser.parse_args()
@@ -36,8 +42,5 @@ if __name__ == '__main__':
 	# Then create the data-base using the schema defined above
 	user, password, host, db = sql_connect().values()
 	engine = create_engine("mysql+pymysql://" + user + ":" + password + "@" + host + "/" + db)
-
-	# Put the FTO no. in the queue here
-	for block, path in zip(block_list, paths):
-		put_fto_nos(block, engine, path)
+	put_fto_nos(block, engine, path)
 
