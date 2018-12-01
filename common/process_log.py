49c9ea537056a0f03638a0d483990da35ae16d0a
akshatgoel92 <akshatgoel92@users.noreply.github.com>
2018-12-01 21:18:58 +0530

    Fix syntax error in Dropbox file format in process log file

--- common/process_log.py
+++ common/process_log.py
@@ -21,7 +21,7 @@ if __name__ == '__main__':
 	# Parse arguments
 	args = parser.parse_args()
 	file_from = args.file_from
-	file_to = args.file_to + '_' str(datetime.today()) + '.csv'
+	file_to = args.file_to + '_' + str(datetime.today()) + '.csv'
 
 	# Call the function
 	process_log(file_from, file_to )
