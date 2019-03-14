375b45b11023c98c73e0c6cfbb6280d3077a6bcd
akshatgoel92 <akshatgoel92@hotmail.com>
2019-03-14 17:17:57 +0530

    Add first pass at reornized FTO code

--- fto_db/download.py
+++ fto_db/download.py
@@ -1,4 +1,3 @@
-
 import os
 import json
 import sys
@@ -10,6 +9,7 @@ import argparse
 import pandas as pd
 import numpy as np
 
+from common import helpers
 from smtplib import SMTP
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
@@ -22,21 +22,18 @@ pymysql.install_as_MySQLdb()
 
 
 # Upload data to Dropbox
-def upload_data(block, path_from = '', path_to = '', to_dropbox = 0):
+def download_data(block, path_from = '', path_to = ''):
 
 	get_transactions = "SELECT * FROM transactions where fto_no in (SELECT fto_no from " + block + ");"
 	get_banks = "SELECT * FROM banks;"
 	get_accounts = "SELECT * from accounts;"
-
-
 	file_from = path_from + block + '.csv'
-
+	
 	user, password, host, db = helpers.sql_connect().values()
 	engine = create_engine("mysql+pymysql://" + user + ":" + password + "@" + host + "/" + db)
 	conn = engine.connect()
 
 	try: 
-		
 		transactions = pd.read_sql(get_transactions, con = conn)
 		banks = pd.read_sql(get_banks, con = conn)
 		accounts = pd.read_sql(get_accounts, con = conn)
@@ -44,12 +41,10 @@ def upload_data(block, path_from = '', path_to = '', to_dropbox = 0):
 		conn.close()
 
 	except Exception as e:
-		
 		print(e)
 		conn.close()
 		
 	try: 
-		
 		transactions = pd.merge(transactions, banks, 
 								how = 'left', 
 								on = ['ifsc_code'], 
@@ -63,28 +58,16 @@ def upload_data(block, path_from = '', path_to = '', to_dropbox = 0):
 		print('Accounts merge done..')
 
 	except Exception as e: 
-		
 		print(e)
 		print('Merge failed...please check the merge.')
 
 	try: 
-		
 		transactions.to_csv(file_from, index = False)
 		print('Transactions sent to .csv...')
 	
 	except Exception as e:
-		
 		print(e) 
 		print('Sending data to .csv failed...please check the .csv upload.')
-	
-	if to_dropbox == 1:
-
-		try:
-			helpers.dropbox_upload(file_from, file_to)
-	
-		except Exception as e:
-			print('Dropbox upload failed...please check the Dropbox upload.')
-
 
 if __name__ == '__main__':
 
@@ -103,4 +86,11 @@ if __name__ == '__main__':
     file_to = ''
     
     # Call function
-    upload_data(block, file_from, file_to, to_dropbox)
+    download_data(block, file_from, file_to, to_dropbox)
+
+	try:
+		helpers.dropbox_upload(file_from, file_to)
+	
+	except Exception as e:
+		print(e)
+		print('Dropbox upload failed...please check the Dropbox upload.')
