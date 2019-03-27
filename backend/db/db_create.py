c7c93a4632f32e922f5980bdeaf985ed809e2c8b
akshatgoel92 <akshatgoel92@hotmail.com>
2019-03-27 13:43:43 +0530

    Remove extra whitespace in db_create.py

--- backend/db/db_create.py
+++ backend/db/db_create.py
@@ -12,7 +12,6 @@ from common import helpers
 def stage_names_execute():
 
 	engine = helpers.db_engine()
-
 	db_schema.create_stage_table_names()
 
 	return
@@ -23,7 +22,6 @@ def stage_names_execute():
 def db_execute(branch = 1):
 
 	engine = helpers.db_engine()
-
 	db_schema.db_create(engine, branch)
 
 	return
@@ -34,11 +32,8 @@ def db_execute(branch = 1):
 def stage_tables_execute():
 
 	stages = db_schema.load_stage_table_names()
-
 	engine = helpers.db_engine() 
-
 	conn = engine.connect()
-	
 	trans = conn.begin()
 	
 	for stage in stages:
@@ -50,11 +45,8 @@ def stage_tables_execute():
 		except Exception as e:
 				
 			print(e)
-				
 			print('Error making the stage table for...:' + stage)
-				
 			trans.rollback()
-				
 			sys.exit()
 
 	trans.commit()
@@ -68,7 +60,6 @@ def stage_tables_execute():
 def primary_key_execute():
 
 	engine = helpers.db_engine()
-
 	db_schema.make_primary_key(engine, 'banks', 'ifsc_code')
 
 	return
@@ -77,9 +68,7 @@ def primary_key_execute():
 def main():
 
 	stage_names_execute()
-
 	db_execute()
-	
 	stage_tables_execute()
 
 
