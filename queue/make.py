921ef305b4c75a79a84d12b537aedacda00fe8ad
akshatgoel92 <akshatgoel92@hotmail.com>
2019-04-15 15:53:27 +0530

    Added description to get_dummies function in queue maker script

--- queue/make.py
+++ queue/make.py
@@ -39,7 +39,7 @@ def prep_csv(stage):
 	return(df)
 
 
-# Create a list of all FTOs by stage	
+# Create a long format data-frame of all FTOs by stage	
 def get_csv(stages):
 	
 	scraped_ftos = pd.concat(map(prep_stage, stages))
@@ -50,14 +50,15 @@ def get_csv(stages):
 
 
 # Calculate the current stage
+# Create dummy variables to get numeric values for whether an FTO has been finished at each stage
 def get_pivoted_stage(fto_stages):
 	
-	# Create dummy variables to get numeric values
+	
 	fto_stages_dum = pd.get_dummies(fto_stages, columns = ['stage'])
 	fto_stages_dum.drop(['fto_no'], inplace = True, axis = 1)
 	fto_stages = pd.concat([fto_stages, fto_stages_dum], axis = 1)
 
-	# Reshape the data-set
+	
 	fto_stages['total'] = 1	
 	fto_stages = fto_stages.pivot_table(index='fto_no', columns='stage', values='total', fill_value=0)
 
@@ -65,22 +66,19 @@ def get_pivoted_stage(fto_stages):
 
 
 # Prep the queue for insert
+# Create the missing columns columns
+# Create a current stage column
+# Iterate through the data-set and create the current stage variable
+# Return the data-frame ready to be merged with the original data-set
 def prep_queue_for_insert(fto_stages, stages, missing_stages):
 	
-	# Create the missing columns columns
+	
 	for col in missing_stages: fto_stages[col] = 0
 
-	# Store the stages in order because it matters here
-	# This will go in a .config file
-	stages=['fst_sig_not', 'fst_sig', 'sec_sig_not', 'sec_sig','sb', 'pb', 'pp', 'P']
-
-	# Correct the index
 	fto_stages.reset_index(inplace=True)
 	fto_stages['current_stage'] = ''
 
-	# Allocate the current stage
 	for stage in stages: fto_stages.loc[fto_stages[stage] == 1, 'current_stage'] = stage
-
 	fto_stages = fto_stages(['fto_no','current_stage'], axis=1)
 	
 	return(fto_stages)
@@ -89,8 +87,8 @@ def prep_queue_for_insert(fto_stages, stages, missing_stages):
 # Now we get the new FTOs along with their current stage
 def get_new_ftos(engine, fto_stages, file_to):
 	
-	fto_queue = db_schema.select_data(engine, 'fto_queue', cols = ['fto_no'])
-	new_ftos = db_schema.anti_join(fto_stages, fto_queue, on = ['fto_no'])
+	fto_queue = schema.select_data(engine, 'fto_queue', cols = ['fto_no'])
+	new_ftos = schema.anti_join(fto_stages, fto_queue, on = ['fto_no'])
 	new_ftos.to_csv(file_to, index = False)
 
 	return
@@ -111,7 +109,7 @@ def main():
 
 	# Set up
 	# Need to change this JSON
-	stages = db_schema.load_stage_table_names()
+	stages = schema.load_stage_table_names()
 	user, password, host, db = helpers.sql_connect().values()
 	engine = create_engine("mysql+pymysql://" + user + ":" + password + "@" + host + "/" + db)
 	
