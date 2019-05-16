29d1d05e27b2e3ecc85b56e7da3fe643a43d49a3
akshatgoel92 <akshatgoel92@hotmail.com>
2019-05-16 19:25:47 +0530

    Enforce repo style in common/errors.py

--- common/errors.py
+++ common/errors.py
@@ -4,6 +4,7 @@ from common import helpers
 
 def output_error(subj, msg):
 
+    
     helpers.send_email(subj, msg)
 
     return
@@ -11,12 +12,13 @@ def output_error(subj, msg):
 
 def handle_error(error_code, data={}):
 
+
     with open('./common/error_messages.json') as data_file:
         error_data = json.load(data_file)
 
     subj = error_data[0][error_code]['subj'].format(**data)
     msg = error_data[0][error_code]['msg'].format(**data)
-
-    output_error(subj, msg)
+	
+	output_error(subj, msg)
 
     return
