5061454c8acabc2f190b38ce22d25bd7d7718455
akshatgoel92 <akshatgoel92@hotmail.com>
2019-08-05 17:08:23 +0530

    Add first pass at reworked branch scrape

--- scrape/pipelines.py
+++ scrape/pipelines.py
@@ -1,11 +1,4 @@
 # -*- coding: utf-8 -*-
-
-#---------------------------------------------------------------------# 
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
-#---------------------------------------------------------------------#  
 import os
 import json
 import re
@@ -46,14 +39,9 @@ class FTONoPipeline(object):
 		
 		if isinstance(item, FTONo) and spider.name == 'fto_urls': 
 			
-			if item['fto_no'] is None:
-				raise(DropItem("Block name missing"))
-
-			if item['fto_no'] == '':
-				raise(DropItem("Block name missing"))
-
-			if item['url'] is None:
-				raise(DropItem('IFSC code missing'))
+			if item['fto_no'] is None: raise(DropItem("Block name missing"))
+			if item['fto_no'] == '': raise(DropItem("Block name missing"))
+			if item['url'] is None: raise(DropItem('IFSC code missing'))
 		
 			self.exporter.export_item(item)
 
@@ -63,6 +51,42 @@ class FTONoPipeline(object):
 	def close_spider(self, spider):	
 
 		if spider.name == 'fto_urls':
+			self.exporter.finish_exporting()
+			self.file.close()
+
+
+
+class FTOBranchPipeline(object):
+	'''This writes the output of FTO urls to a set of .csvs.'''
+
+
+	def open_spider(self, spider):
+	
+		if spider.name == 'fto_branch':
+			
+			self.file = open('./output/transactions_alt' + '.csv', 'w+b')
+			self.exporter = CsvItemExporter(self.file)
+			self.exporter.start_exporting()
+
+
+	def process_item(self, item, spider):
+		
+		if isinstance(item, FTOItem) and spider.name == 'fto_branch': 
+			
+			if item['block_name'] is None: raise(DropItem("Block name missing"))
+			if item['ifsc_code'] == "Total": raise(DropItem("IFSC code missing"))
+			if item['wage_list_no'] is None: raise(DropItem("Wage list no. missing"))
+			if item['wage_list_no'] == '': raise(DropItem("Wage list no. missing"))
+			if re.search('\d{10}NRG\d{17}', item['transact_ref_no']) is None: raise(DropItem("Transaction ref no does not fit format"))
+		
+			self.exporter.export_item(item)
+
+		return(item)
+
+
+	def close_spider(self, spider):	
+
+		if spider.name == 'fto_branch':
 			self.exporter.finish_exporting()
 			self.file.close()
 
@@ -84,23 +108,22 @@ class FTOContentPipeline(object):
 	
 	def process_item(self, item, spider):
 
+		
 		if isinstance(item, FTOOverviewItem) and spider.name == 'fto_content':
 			
 			title_fields = ['state', 'district', 'block', 'type', 'pay_mode']
 			item = clean_item(item, title_fields)
 			sql,  data = update_fto_type(item['fto_no'], item['fto_type'], str(spider.block))
 			self.dbpool.runOperation(sql, data)
-		 
+		
+		
 		if isinstance(item, FTOItem) and spider.name == 'fto_content':
 
-			title_fields = ['block_name,' 'status', 'rejection_reason', 'prmry_acc_holder_name',
-							'app_name']
+			title_fields = ['block_name,' 'status', 'rejection_reason', 'prmry_acc_holder_name', 'app_name']
 			tables = ['banks', 'transactions', 'wage_lists', 'accounts']
 			unique_tables = ['banks', 'wage_lists', 'accounts']
 			
-			if item['block_name'] is None:
-				raise(DropItem("Block name missing"))
-			
+			if item['block_name'] is None: raise(DropItem("Block name missing"))
 			item = clean_item(item, title_fields)
 			
 			for table in tables:
@@ -109,51 +132,12 @@ class FTOContentPipeline(object):
 				keys = get_keys(table)
 				sql, data = insert_data(item, keys, table, unique)
 				
-				try:
-					self.dbpool.runOperation(sql, data)
-				
-				except Exception as e:
-					self.logger.error('Error in the data-base upload: %s', str(e))
-
-
-		if isinstance(item, FTOItem) and spider.name == 'fto_branch':
-			
-			title_fields = ['block_name', 'app_name', 'status', 'rejection_reason']
-			tables = ['banks', 'transactions', 'wage_lists']
-			unique_tables = ['banks', 'wage_lists']
-			
-			if item['block_name'] is None:
-				raise(DropItem("Block name missing"))
+				try: self.dbpool.runOperation(sql, data)
+				except Exception as e: self.logger.error('Error in the data-base upload: %s', str(e))
 		
-			if item['ifsc_code'] == "Total":
-				raise(DropItem("IFSC code missing"))
-		
-			if item['wage_list_no'] is None:
-				raise(DropItem("Wage list no. missing"))
-		
-			if item['wage_list_no'] == '':
-				raise(DropItem("Wage list no. missing"))
-			
-			if re.search('\d{10}NRG\d{17}', item['transact_ref_no']) is None:
-				raise(DropItem("Transaction ref no does not fit format"))
-				
-			item = clean_item(item, title_fields)	
-			
-			for table in tables:
-				
-				unique = 1 if table in unique_tables else 0
-				keys = get_keys(table)
-				sql, data = insert_data(item, keys, table, unique)
-				
-				try:
-					self.dbpool.runOperation(sql, 
-												data)
-				except Exception as e:
-					self.logger.error('Error in the data-base upload: %s', str(e))
-					
 		return(item)
 	
-
+	
 	def close_spider(self, spider):
 		self.dbpool.close()
 
@@ -161,7 +145,7 @@ class FTOContentPipeline(object):
 
 class FTOMaterialPipeline(object):
 
-
+	
 	def open_spider(self, spider):
 	
 		if spider.name == 'fto_material':
@@ -178,18 +162,12 @@ class FTOMaterialPipeline(object):
 		# This sequence of statements removes these rows from the final scrape
 		if isinstance(item, FTOMaterialItem) and spider.name == 'fto_material': 
 			
-			if item['block_name'] is None:
-				raise(DropItem("Block name missing"))
-
-			if item['block_name'] == '':
-				raise(DropItem("Block name missing"))
-
-			if item['ifsc_code'] is None:
-				raise(DropItem('IFSC code missing'))
-		
-			if item['ifsc_code'] == "Total":
-				raise(DropItem("IFSC code missing"))
-
+			if item['block_name'] is None: raise(DropItem("Block name missing"))
+			if item['block_name'] == '': raise(DropItem("Block name missing"))
+			
+			if item['ifsc_code'] is None: raise(DropItem('IFSC code missing'))
+			if item['ifsc_code'] == "Total": raise(DropItem("IFSC code missing"))
+			
 			self.exporter.export_item(item)
 			
 		return(item)
