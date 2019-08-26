4335a9f24a801200083a0673e50fd1f6b9a9d63c
akshatgoel92 <akshatgoel92@hotmail.com>
2019-08-26 12:43:08 +0530

    Move utils script into its own folder

--- script/utils/utils.py
+++ script/utils/utils.py
@@ -97,56 +97,6 @@ def format_indicators(df):
 	return(df)
 	
 
-def set_static_scripts(df):
-			
-	# Not got welcome script and not got static NREGA introduction so should get the welcome script
-	df.loc[(df['got_static_nrega'] == 0) & (df['got_welcome'] == 0), 'day1'] = "P0 P1 P2 00 P0"
-	
-	# Got welcome script but not got static NREGA introduction so should get static NREGA introduction
-	df.loc[(df['got_static_nrega'] == 0) & (df['got_welcome'] == 1), 'day1'] = "P0 P1 P2 P3 Q A P0 Z1 Z2"
-	
-	# Proportional wages
-	df.loc[(df['got_static_nrega'] == 1), 'day1'] = 'P0 P1 P2 P3 Q B P0 Z1 Z2'
-	
-	return(df)
-
-
-def set_nrega_scripts(df):
-	
-	# Dynamic NREGA scripts for FTOs at the block office
-	df.loc[(df['stage']=='fst_sig_not') | (df['stage']=='fst_sig'), 'day1'] = 'P0 P1 P2 P3 R CA CB CC P0 Z1 Z2'
-	df.loc[(df['stage']=='sec_sig_not') | (df['stage']=='sec_sig'), 'day1'] = 'P0 P1 P2 P3 R CA CB CC P0 Z1 Z2'
-	
-	# Dynamic NREGA scripts for unprocessed FTOs at the bank
-	df.loc[(df['stage']=='sb') | (df['stage']=='pp') | (df['stage']=='P'), 'day1'] = 'P0 P1 P2 P3 R DA DB DC P0 Z1 Z2'
-		
-	# Dynamic NREGA scripts for transactions which have been processed
-	df.loc[(df['status']=='Processed') & (df['stage']=='pb'), 'day1'] = 'P0 P1 P2 P3 R EA EB EC P0 Z1 Z2'
-	df.loc[(df['status']=='Rejected') & (df['stage']=='pb'), 'day1'] = 'P0 P1 P2 P3 R FA FB FC P0 Z1 Z2'
-	
-	return(df)
-
-
-def set_nrega_hh_amounts(df):
-	
-	# Replace amount with actual amount if it exists else with the amount due
-	df['amount'] = np.where(df['credit_amt_actual'] != 0, df['credit_amt_actual'], df['credit_amt_due'])
-	df['amount'] = df.groupby('id')['amount'].transform('sum')
-	df['amount'] = df['amount'].replace(0, np.nan)
-	
-	return(df)
-	
-	
-def set_nrega_hh_dates(df):
-	
-	# Replace transact_date with processed_date if transaction has been processed and then format
-	df['transact_date'] = np.where(~df['processed_date'].isna(), df['processed_date'], df['transact_date'])
-	df['transact_date'] = pd.to_datetime(df['transact_date'], format = '%Y/%m/%d', dayfirst = True)
-	df['transact_date'] = df.groupby('id')['transact_date'].transform('max')
-	
-	return(df)
-
-
 def format_df(df, static):
 	
 	if static == 1: 
@@ -169,3 +119,16 @@ def add_test_calls(df):
 	df = pd.concat([df, test_calls])
 	
 	return(df)
+
+
+def output_df(df, static, dynamic, join):
+	
+	# Output scripts
+	if join == 1:
+		df = pd.concat([df_dynamic, df_static])
+		df.to_csv(local_output_path, index = False)
+	
+	elif static == 1: df_static.to_csv(local_output_path, index = False)
+	elif dynamic == 1: df_dynamic.to_csv(local_output_path, index = False)
+	
+	return(join, static, dynamic)
